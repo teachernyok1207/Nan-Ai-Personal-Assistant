fac410599ff8f33944924cba5db769f7c215a400
Norman Galanza <norman.galanza@gmail.com>
2024-01-10 18:23:08 +0800

    modified:   utils/retriever.js

--- utils/retriever.js
+++ utils/retriever.js
@@ -2,11 +2,11 @@ import { SupabaseVectorStore } from 'langchain/vectorstores/supabase'
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
 import { createClient } from '@supabase/supabase-js'
 
-const openAIApiKey = process.env.OPENAI_API_KEY
+const openAIApiKey = import.meta.env.VITE_OPENAI_API_KEY
 
 const embeddings = new OpenAIEmbeddings({ openAIApiKey })
-const sbApiKey = process.env.SUPABASE_API_KEY
-const sbUrl = process.env.SUPABASE_URL_LC_CHATBOT
+const sbApiKey = import.meta.env.VITE_SUPABASE_API_KEY
+const sbUrl = import.meta.env.VITE_SUPABASE_URL_LC_CHATBOT
 const client = createClient(sbUrl, sbApiKey)
 
 const vectorStore = new SupabaseVectorStore(embeddings, {
